704d7bf71bc1cf833c3de5357dee6c33363e1322
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-03-01 20:10:07 -0300

    Add files via upload

--- mysite/models.py
+++ mysite/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.shortcuts import render
+from django import forms
 # Create your models here.
 
 class Curso(models.Model):
@@ -11,3 +12,18 @@ class Curso(models.Model):
     def __str__(self):
         texto = "{0} ({1})"
         return texto.format(self.nombre, self.creditos)
+    
+class Curso1(models.Model):
+    def your_view_name(request):
+        print('river')
+        if request.method == 'POST':
+            print('river')
+            if request.post('button_clicked' == 'clicked'):
+                variable = 1 
+            
+            else:
+                variable = 2
+                print('river')
+        else:
+            print('aca')
+        return render(request,'1.html', {"variable": variable})
