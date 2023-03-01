a7dbc4df0ce698d3fdba65ed47238d0277fd1b17
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-03-01 20:07:12 -0300

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,14 @@ urlpatterns = [
     # - Homepage
 
     path('', views.home, name=""),
-    path('registrarcurso/', views.registrarCurso)
+    path('bajo.html/', views.bajo,name='bajo'),
+    path('medio.html/', views.medio,name='medio'),
+    path('alto.html/', views.alto,name='alto'),
+    path('test.html/', views.your_view_name,name='button_clicked'),
+    
+    
+    
+    #path('registrarcurso/', views.registrarCurso)
 
 
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import Curso
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Curso1
 # - Homepage
 
 def home(request):
@@ -14,3 +17,43 @@ def registrarCurso(request):
     curso = Curso.objects.create(nombre=email, codigo=name, creditos=creditos)
     
     return render(request,"templates/index.html",{})
+
+def bajo(request):
+    cursos = Curso.objects.all()
+    return render(request, "bajo.html", {"cursos": cursos})
+
+def medio(request):
+    cursos = Curso.objects.all()
+    return render(request, "medio.html", {"cursos": cursos})
+
+def alto(request):
+    print('river')
+    return render(request, "alto.html")
+
+def test(request):
+    
+    print('test')
+    return render(request, "test.html")
+
+
+
+
+
+
+
+def your_view_name(request):
+    if request.method == 'POST':
+        if request.POST.get('button_clicked') == 'clicked' and request.POST.get('checkbox3'):
+            print('river')
+            pass
+        else:
+            print('river2')
+            # Do something else if the button is clicked but the checkbox is not checked,
+            # or if the button is not clicked at all
+            pass
+
+    return render(request, 'test.html')
+
+
+
+
